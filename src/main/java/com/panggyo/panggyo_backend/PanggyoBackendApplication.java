ced90cf4a85e8ef018a35884e5cabe532fee1cef
jieunyy <ina011011@naver.com>
2025-02-26 15:20:38 +0900

    feat: initial setting done

---
+++ src/main/java/com/panggyo/panggyo_backend/PanggyoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.panggyo.panggyo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanggyoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanggyoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panggyo/panggyo_backend/PanggyoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panggyo.panggyo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanggyoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
